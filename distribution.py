fc6da3c30d304caaff3f6a4e517c18b28bfd8a7c
APikielny <apikielny@gmail.com>
2015-09-23 13:49:22 -0400

    Updated from Brython Server: September 23, 2015 1:49:23 PM EDT

--- distribution.py
+++ distribution.py
@@ -45,8 +45,9 @@ for letter in alphabet:
     dictionary[letter]=0
 for letter in lowercase:
     dictionary[letter]+=1
+
 #print (dictionary)
-print("The distribution of characters in '" + string + "'")
+#print("The distribution of characters in '" + string + "'")
 alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
 for letter in alphabet:
     if dictionary[letter]!=0:
